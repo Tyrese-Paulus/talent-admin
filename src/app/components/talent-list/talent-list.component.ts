4c7379ffda32e55845fb75e0e85dd32db81ea3ad
Tyrese-Paulus <tpwebdevelopment11@gmail.com>
2023-01-29 13:51:47 +0200

    sidebar almost complete

---
+++ src/app/components/talent-list/talent-list.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Talent } from './../../models/talent';
+import { Router } from '@angular/router';
+
+import { TalentService } from 'src/app/services/talent-service/talent.service';
+
+
+
+@Component({
+  selector: 'app-talent-list',
+  templateUrl: './talent-list.component.html',
+  styleUrls: ['./talent-list.component.scss']
+})
+export class TalentListComponent implements OnInit {
+
+    talents: Talent[] = [];
+
+  constructor(private talentService: TalentService, private router: Router) { }
+
+  ngOnInit(): void {
+      this.talentService.getTalents().subscribe((talent) => {
+      this.talents = talent
+    })
+  }
+
+  updateTalent(talentid: string){
+    this.router.navigateByUrl(`talents/form/${talentid}`)
+  }
+
+}
---
+++ src/app/models/talent.ts
@@ -0,0 +1,14 @@
+export class Talent {
+  id?: string;
+  name?: string;
+  height?: number;
+  bust?: number;
+  image?: string;
+  images?: string[];
+  waist?: number;
+  hips?: number;
+  dress?: number;
+  shoe?: number;
+  hair?: string;
+  eyes?: string;
+}
